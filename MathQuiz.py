850640a1a7d54f1cb1c79c9a1e0a075929779eb6
Sabir Mohammedi Taieb <52837091+Sabir97@users.noreply.github.com>
2022-12-19 10:01:03 +0000

    Added Score, Time, CorrectAnswers Number variables

--- MathQuiz.py
+++ MathQuiz.py
@@ -1,9 +1,12 @@
 # Math Quiz by Sabir Mohammedi Taieb
 import random
 
+time = 0
+score = 0
+correctAns = 0
 userSum = 0
-print("# Math Quiz by Sabir Mohammedi Taieb")
-
+print("# Math Quiz by Sabir Mohammedi Taieb \n")
+print("Write -1 to exit.")
 while (userSum != -1):
   n1 = random.randint(0, 100)
   n2 = random.randint(0, 100)
@@ -15,5 +18,10 @@ while (userSum != -1):
 
   if (userSum == Sum):
     print("Correct")
+    score += 1
+  if (userSum == -1):
+    break
   else:
     print("Wrong answer! Try again, the correct answer is: ", Sum)
+
+print("\n your score is: ", correctAns)
