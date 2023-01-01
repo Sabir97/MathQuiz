cbb98cec61852671bd579c0ccbb4941fac279998
Sabir97 <mohammeditaiebsabir@gmail.com>
2023-01-01 18:20:18 +0100

    Changed time output from seconds to minutes

--- MathQuiz.py
+++ MathQuiz.py
@@ -8,7 +8,7 @@ correctAns = 0
 userSum = 0
 #start = 0.0
 #finish = 0.0
-print("# Math Quiz by Sabir Mohammedi Taieb \n")
+print("# Math Quiz CLI by Sabir Mohammedi Taieb \n")
 print("Write -1 to exit.")
 
 start = time.perf_counter()
@@ -31,10 +31,10 @@ while (userSum != -1):
 
 finish = time.perf_counter()
 time = finish - start
-score = correctAns - time
+score = correctAns - time/60
 if(correctAns==0):
   score=0
 
 print(f"\n Your time was: {time:0.2f} sec. Number of correct answers is:", correctAns, ". Your Score is: ",score)
 
-input("\n Press any key to exit ...")
+input("\n Press Enter to exit ...")
